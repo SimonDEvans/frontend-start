e65ef87c4b770175745bad87715fe6e760bc80b9
Simon Evans <simon.evans@torchbox.com>
2017-03-29 16:38:16 +0100

    initial commit

---
+++ site/javascript/main.js
@@ -0,0 +1,32 @@
+
+/*
+    Example of importing and using classes
+*/
+
+import MobileMenu from './components/MobileMenu';
+
+let mobileNavigation = new MobileMenu({
+    target      : '#app__nav',
+    inSpeed     : 900,
+    outSpeed    : 900
+});
+
+console.log( 'hello', mobileNavigation );
+
+
+/*
+    Example of importing and using functions
+*/
+
+import $ from './globals';
+import positionSticky from './utility/position-sticky';
+
+positionSticky( $('.cards__title'), $( '.app__page > .content' ) );
+
+$('.cards__title').on( 'stuck', function( e ){
+    console.log( 'ON, Demonstrate custom events on elements', e );
+});
+
+$('.cards__title').on( 'unstuck', function( e ){
+    console.log( 'OFF, Demonstrate custom events on elements', e );
+});
